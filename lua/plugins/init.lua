f236b00c033322d561fedf2802bd70fdea6e15c2
Jaydin Gulley <jaydin.gulley@outlook.com>
2024-10-13 22:35:34 -0500

    latest

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -114,7 +114,22 @@ return {
       require("spectre").setup()
     end,
   },
+  -- {
+  --   "nvim-lua/plenary.nvim",
+  -- },
   {
-    "nvim-lua/plenary.nvim",
-  },
+    "NeogitOrg/neogit",
+    dependencies = {
+      "nvim-lua/plenary.nvim",  -- required
+      "sindrets/diffview.nvim", -- optional - Diff integration
+      -- Only one of these is needed.
+      -- "nvim-telescope/telescope.nvim", -- optional
+      -- "ibhagwan/fzf-lua",            -- optional
+      -- "echasnovski/mini.pick",       -- optional
+    },
+    config = function()
+      require('neogit').setup {}
+    end
+  }
+
 }
